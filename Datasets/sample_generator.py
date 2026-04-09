934ee6db8ed81318edc6a0d82f69ee7c85e797de
Sakib Hossain <sakibhossainalif29@gmail.com>
2026-04-09 20:17:28 +0600

    Add docstring documentation for `sample_generator.py`

--- Datasets/sample_generator.py
+++ Datasets/sample_generator.py
@@ -1,3 +1,14 @@
+"""
+sample_generator.py
+
+This script is used to generate samples directly from the main dataset. 
+These samples are used to test the `/predict/file` route. 
+To run it, enter this in your command line:
+```
+python -m Datasets.sample_generator.py
+```
+"""
+
 import pandas as pd
 from pathlib import Path
 
@@ -5,25 +16,20 @@ DATASET_PATH = Path("Datasets","SDSS_DR18.csv")
 OUTPUT_PATH = Path("Datasets","samples.csv")
 
 def preprocess(df: pd.DataFrame) -> pd.DataFrame:
-    # Drop irrelevant columns (ignore if already absent)
     drop_cols = ["objid", "specobjid", "run", "rerun", "camcol",
                  "field", "plate", "mjd", "fiberid"]
     df = df.drop(columns=[c for c in drop_cols if c in df.columns])
 
-    # Map string classes to numeric labels
     df["class"] = df["class"].map({"GALAXY": 0, "STAR": 1, "QSO": 2})
 
-    # Feature reduction
     df = df[["ra", "dec", "redshift", "u", "g", "r", "i", "z", "psfMag_r", "class"]].copy()
 
-    # Feature engineering — color contrast columns
     df["u_g_color"] = df["u"] - df["g"]
     df["g_r_color"] = df["g"] - df["r"]
     df["r_i_color"] = df["r"] - df["i"]
     df["i_z_color"] = df["i"] - df["z"]
     df = df.drop(columns=["u", "g", "r", "i", "z"])
 
-    # Move `class` to the end
     popped_class = df.pop("class")
     df.insert(len(df.columns), "class", popped_class)
 
@@ -34,7 +40,6 @@ def stratified_sample(df: pd.DataFrame, total_samples: int, class_col: str = "cl
     class_counts = df[class_col].value_counts(normalize=True)
     class_n = (class_counts * total_samples).round().astype(int)
 
-    # Fix rounding drift
     diff = total_samples - class_n.sum()
     class_n.iloc[0] += diff
 
@@ -43,7 +48,6 @@ def stratified_sample(df: pd.DataFrame, total_samples: int, class_col: str = "cl
     )
 
 
-# --- Usage ---
 df_raw = pd.read_csv(DATASET_PATH)
 
 df_processed = preprocess(df_raw)
